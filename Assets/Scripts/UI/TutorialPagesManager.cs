e0e7aec4fa1b3c4c524baa31cc7c34302c491544
Antonio Feregrino <antonio.feregrino@gmail.com>
2026-02-09 20:43:49 +0000

    Add tutorial scene

---
+++ Assets/Scripts/UI/TutorialPagesManager.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialPagesManager : MonoBehaviour
+{
+
+    private GameObject[] tutorialPages;
+
+    private int currentPage = 0;
+
+    public Button backButton;
+    public Button nextButton;
+
+    private TextMeshProUGUI backButtonText; 
+    private TextMeshProUGUI nextButtonText; 
+
+    void Awake()
+    {
+        var allChildren = GetComponentsInChildren<Transform>(true);
+        foreach (var child in allChildren)
+        {
+            Debug.Log($"Child: {child.name}");
+        }
+        var pages = allChildren.Where(
+            child => child.name.StartsWith("Page") && child.parent == this.transform).Select(child => child.gameObject);
+        // Sort pages by number
+        pages = pages.OrderBy(page => int.Parse(page.name.Substring(4)));
+        tutorialPages = pages.ToArray();
+
+        currentPage = 0;
+        ShowPage(currentPage);
+
+        backButton.onClick.AddListener(OnBackButtonClick);
+        nextButton.onClick.AddListener(OnNextButtonClick);
+
+        backButtonText = backButton.GetComponentInChildren<TextMeshProUGUI>();
+        nextButtonText = nextButton.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    void OnBackButtonClick()
+    {
+        if (currentPage == 0)
+        {
+            // TODO: Go back to the previous scene
+            return;
+        }
+        currentPage--;
+        ShowPage(currentPage);
+    }
+
+    void OnNextButtonClick()
+    {
+        if (currentPage == tutorialPages.Length - 1)
+        {
+            // TODO: Go back to the previous scene
+            return;
+        }
+        currentPage++;
+        ShowPage(currentPage);
+    }
+
+    public void ShowPage(int pageNumber)
+    {
+        Debug.Log($"Showing page {pageNumber}");
+        for (int i = 0; i < tutorialPages.Length; i++)
+        {
+            tutorialPages[i].SetActive(i == pageNumber);
+        }
+
+        if (pageNumber == 0)
+        {
+            backButtonText.text = "Exit";
+        }
+        else
+        {
+            backButtonText.text = "Back";
+        }
+        if (pageNumber == tutorialPages.Length - 1)
+        {
+            nextButtonText.text = "Exit";
+        }
+        else
+        {
+            nextButtonText.text = "Next";
+        }
+    }
+}
