23925d575ed78bd4cf5ae49854c2cca3cd57d0a0
Antonio Feregrino <antonio.feregrino@gmail.com>
2026-02-09 21:23:34 +0000

    Switch thing for awake

--- Assets/Scripts/UI/CardButton.cs
+++ Assets/Scripts/UI/CardButton.cs
@@ -41,7 +41,7 @@ public class CardButton : MonoBehaviour, UnityEngine.EventSystems.IPointerDownHa
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         var allChildren = GetComponentsInChildren<Transform>();
 
